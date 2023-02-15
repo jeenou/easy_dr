2e40f4c1e38e1b1a1f0e526704a7f757f74ddca7
jeenou <essi.nousiainen@vtt.fi>
2023-02-15 11:34:46 +0200

    Added open_predicer

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod main_loop;
 pub fn start_sending(tx: Sender<main_loop::_Task>) {
     
     tx.send(main_loop::_Task::StartProcess).unwrap();
-    
+    tx.send(main_loop::_Task::QuitProcess).unwrap();
+    tx.send(main_loop::_Task::StartProcess).unwrap();
 }
 
 fn main() {
--- src/main_loop.rs
+++ src/main_loop.rs
@@ -1,6 +1,8 @@
 
 use std::sync::mpsc::{Receiver};
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::io::Write;
+use std::path::PathBuf;
 
 pub enum _Task {
     StartProcess,
@@ -8,13 +10,25 @@ pub enum _Task {
 }
 
 pub fn task_loop(rx: Receiver<_Task>) {
-    for received in rx {
-        match received {
-            _Task::StartProcess => {
-                _start();
-            },
-            _Task::QuitProcess => {
-                println!("quit process");
+    let mut running = true;
+
+    while running {
+        match rx.try_recv() {
+            Ok(received) => {
+                match received {
+                    _Task::StartProcess => {
+                        println!("start process");
+                        _start();
+
+                    },
+                    _Task::QuitProcess => {
+                        println!("quit process");
+                        running = false;
+                    }
+                }
+            }
+            Err(_) => {
+                break;
             }
         }
     }
@@ -29,15 +43,43 @@ fn _start() {
         logs (dict): process logs
     */
 
-    _create_process();
+    open_predicer();
     
 }
 
 fn _create_process() {
-    //Palauttaa prosessin
+    //Starts a new process.
     Command::new("mspaint")
     .spawn()
     .expect("failed to start paint program");
+}
+
+fn open_predicer() {
+    //Starts Predicer.
+
+    let mut path = PathBuf::from("src");
+    path.push("Predicer");
+
+    let mut child = Command::new("julia")
+    .current_dir(path)
+    .args(&["--eval", "using Pkg; Pkg.activate(\".\"); Pkg.instantiate();"])
+    .stdin(Stdio::piped())
+    .spawn()
+    .expect("failed to execute process");
+
+    let input = b"]\nactivate .\nbackspace\nusing Predicer\nmc, input_data = Predicer.generate_model(\"input_data/input_data.xlsx\")\n";
+    let child_stdin = child.stdin.as_mut().unwrap();
+    child_stdin.write_all(input).unwrap();
+
+    // Wait for Julia to finish executing the commands
+    let output = child.wait_with_output().expect("failed to wait on child");
+
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+
+}
+
+fn _quit_process() {
+    //Terminates a running process
 }
 
 #[test]
